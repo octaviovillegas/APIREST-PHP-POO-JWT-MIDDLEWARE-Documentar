5d4a8f60d22b61d1c23c689c33c50fc24f9e05ea
root <el.octavio.villegas@gmail.com>
2018-05-28 19:32:35 -0300

    inicio desde cero

---
+++ apirestV0/index.php
@@ -0,0 +1,20 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require_once '../composer/vendor/autoload.php';
+
+
+
+$app = new \Slim\App([]);
+
+
+
+$app->get('[/]', function (Request $request, Response $response) {    
+    $response->getBody()->write("GET => Bienvenido!!! ,a SlimFramework");
+    return $response;
+
+});
+
+
+$app->run();
