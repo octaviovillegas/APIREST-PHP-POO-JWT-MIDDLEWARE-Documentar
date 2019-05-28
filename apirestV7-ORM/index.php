59760a232dfb19539f10aad80d5056a69fac6549
octaviovillegas <el.octavio.villegas@gmail.com>
2019-05-27 21:59:30 -0300

    subiendo

--- apirestV7-ORM/index.php
+++ apirestV7-ORM/index.php
@@ -4,10 +4,10 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 
 require '../vendor/autoload.php';
-require_once '../clases/AccesoDatos.php';
-require_once '../clases/cdApi.php';
-require_once '../clases/AutentificadorJWT.php';
-require_once '../clases/MWparaCORS.php';
+require_once './clases/AccesoDatos.php';
+require_once './clases/cdApi.php';
+require_once './clases/AutentificadorJWT.php';
+require_once './clases/MWparaCORS.php';
 require_once './clases/MWparaAutentificar.php';
 use \App\Models\cd as cdORM;
 
